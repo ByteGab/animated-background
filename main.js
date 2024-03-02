8cbe268a7dc0b33e8e4d2fa5ead9943adbd1a800
ByteGab <gabrielfreiresimon@gmail.com>
2024-03-03 00:27:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('.container');
 const colors = ['#CB51EE','#0073BC','#38B75E','#DE365C'];
 
-const figures = () => {
-    for(let i = 0;i <=70; i++ ){
+const figures1 = () => {
+    for(let i = 0;i <=30; i++ ){
         let figure = document.createElement('span');
+        figure.classList.add('cuadrado');
         let select = Math.round(colors.length * Math.random()) % colors.length;
 
         figure.style.top = innerHeight * Math.random() + 'px';
@@ -15,8 +16,28 @@ const figures = () => {
         setInterval(() => {
             figure.style.top = innerHeight * Math.random() + 'px';
             figure.style.left = innerWidth * Math.random() + 'px';
-        },3005)
+        },4005)
+    }
+}
+figures1();
+
+const figures2 = () => {
+    for(let i = 0;i <=30; i++ ){
+        let figure = document.createElement('span');
+        figure.classList.add('circulo');
+        let select = Math.round(colors.length * Math.random()) % colors.length;
+
+        figure.style.top = innerHeight * Math.random() + 'px';
+        figure.style.left = innerWidth * Math.random() + 'px';
+        figure.style.background = colors[select];
+
+        container.appendChild(figure);
+        
+        setInterval(() => {
+            figure.style.top = innerHeight * Math.random() + 'px';
+            figure.style.left = innerWidth * Math.random() + 'px';
+        },4005)
     }
 }
 
-figures();
+figures2();
